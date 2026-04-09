31e3bbc41ae3078973d3514112c4ca4e31b40ceb
Diego Dominguez <diego0220d4@gmail.com>
2026-04-09 16:13:58 -0400

    Update Form nav Link

--- src/nav.tsx
+++ src/nav.tsx
@@ -1,5 +1,6 @@
 import Home from "./home.tsx";
 import About from "./about.tsx";
+import TheForm from "./form.tsx";
 import Products from "./products.tsx";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function Nav() {
             <Routes>
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/form" element={<TheForm />} />
                 <Route path="/products" element={<Products />} />
             </Routes>
         </BrowserRouter>
@@ -23,6 +25,7 @@ function SetNav() {
             <ul>
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
+                <li><Link to="/form">Form</Link></li>
                 <li><Link to="/products">Products</Link></li>
             </ul>
         </nav>
