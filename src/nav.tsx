e49d0b5730357d5a86b4cb09c420deefe8af3b24
Diego Dominguez <diego0220d4@gmail.com>
2026-04-09 13:21:11 -0400

    Latest {<Product />}

---
+++ src/nav.tsx
@@ -0,0 +1,32 @@
+import Home from "./home.tsx";
+import About from "./about.tsx";
+import Products from "./products.tsx";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+
+function Nav() {
+    return (
+        <BrowserRouter>
+            <SetNav />
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/products" element={<Products />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+function SetNav() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/products">Products</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
